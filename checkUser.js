88c6d398e1f6c4742bd7e61b8154a8ad20d1ad77
Felipe Carretero <fjcarretero@Felipes-MacBook-Air.local>
2017-10-15 19:45:04 +0200

    Test users 2

---
+++ checkUser.js
@@ -0,0 +1,50 @@
+var mongoose = require('mongoose'),
+models = require('./models');
+
+var usrs = process.argv.slice(2);
+
+var mongoServiceName = process.env.DATABASE_SERVICE_NAME.toUpperCase();
+mongo = {
+    "hostname": process.env[mongoServiceName + '_SERVICE_HOST'],
+    "port": parseInt(process.env[mongoServiceName + '_SERVICE_PORT']),
+    "username": process.env[mongoServiceName + '_USER'],
+    "password": process.env[mongoServiceName + '_PASSWORD'],
+    "db": process.env[mongoServiceName + '_DATABASE'],
+};
+
+// MongoDB
+
+var generate_mongo_url = function(obj){
+	obj.hostname = (obj.hostname || 'localhost');
+	obj.port = (obj.port || 27017);
+	obj.db = (obj.db || 'test');
+	if(obj.username && obj.password){
+		return "mongodb://" + obj.username + ":" + obj.password + "@" + obj.hostname + ":" + obj.port + "/" + obj.db;
+	}else{
+		return "mongodb://" + obj.hostname + ":" + obj.port + "/" + obj.db;
+	}
+};
+
+var mongourl = generate_mongo_url(mongo);
+console.log(mongourl);
+// Test
+
+models.defineModels(mongoose, function() {
+  User = mongoose.model('User');
+  db = mongoose.connect(mongourl);
+});
+
+User.findOne({ email: usrs[0] }, function(err, user) {
+    console.log('inside findOne');
+    if (err) { 
+        console.log('Error '+ err);
+        return done(err); 
+    }
+    if (user) {
+        user.name = profile.displayName;
+        //user.role = 'admin';
+        console.log('User found');
+    } else {
+        console.log.error('User not found ' + user);
+    }
+});
